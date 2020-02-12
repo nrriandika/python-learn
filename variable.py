2c66ba30b1220b8f219bcb15ecd45062526cdbb4
Novandi Resya Riandika <58996567+nrriandika@users.noreply.github.com>
2020-02-12 12:21:51 +0700

    Update variable.py

--- variable.py
+++ variable.py
@@ -1,5 +1,6 @@
 character_name = "Bella"
 character_age = "23"
+character_hobby = "cook"
 print("There was a girl named" + character_name + ",")
 print("She was" + character_age + " years old. ")
 print("She really likes to read a" + character_hobby + "." 
