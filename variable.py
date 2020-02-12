10e207c3ea973f5fb930a1d1af910c66c99b2360
Novandi Resya Riandika <58996567+nrriandika@users.noreply.github.com>
2020-02-12 12:26:11 +0700

    add another variables

--- variable.py
+++ variable.py
@@ -1,6 +1,11 @@
 character_name = "Bella"
 character_age = "23"
-character_hobby = "cook"
-print("There was a girl named" + character_name + ",")
-print("She was" + character_age + " years old. ")
-print("She really likes to read a" + character_hobby + "." 
+character_hobby = "singing"
+  print("There was a girl named" + character_name + ",")
+  print("She was" + character_age + " years old. ")
+  print("She really likes to read a" + character_hobby + "." 
+character_name = "Nurai"
+character_age = "20"
+character_hobby = "cooking"
+  print(+ character_name + "is a diligent girl,")
+  print("although she only" + character_age + "years old." 
