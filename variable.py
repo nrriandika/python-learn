da946e421737bcbd1d5f23933109fe25345d02be
Novandi Resya Riandika <58996567+nrriandika@users.noreply.github.com>
2020-02-12 12:20:57 +0700

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,5 @@
+character_name = "Bella"
+character_age = "23"
+print("There was a girl named" + character_name + ",")
+print("She was" + character_age + " years old. ")
+print("She really likes to read a" + character_hobby + "." 
